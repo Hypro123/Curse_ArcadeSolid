d0eb3692eb6d7c734ee0cea13d9161aa9e907d1b
AIE\s171649 <eddie.ultra@gmail.com>
2018-05-21 12:08:42 +1000

    Added children block script and base movement scripts

---
+++ Curse of the Arcade Solid/Assets/_Scripts/BossMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    [SerializeField]
+    int ibossSpeed = 10;
+    [SerializeField]
+    string SpawnBossTag = "bossSpawn";
+    [SerializeField]
+    GameObject[] tPositions;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        tPositions = GameObject.FindGameObjectsWithTag(SpawnBossTag);
+        spawn();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.position += this.transform.forward * ibossSpeed * Time.deltaTime;
+	}
+
+    void spawn()
+    {
+        int i = Random.Range(0, tPositions.Length);
+        this.transform.position = tPositions[i].transform.position;
+    }
+}
---
+++ Curse of the Arcade Solid/Assets/_Scripts/ChildBlock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildBlock : MonoBehaviour {
+
+    void Awake()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void setThisInactive()
+    {
+        this.gameObject.SetActive(false);
+        this.GetComponentInParent<BossHP>().TakeDmg();
+    }
+}
