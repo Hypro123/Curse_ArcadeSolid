912a3fdcdc9795b7d8e15b228fed36cc9669dfb2
AIE\s171649 <eddie.ultra@gmail.com>
2018-05-21 11:23:25 +1000

    scripts folder

---
+++ Curse of the Arcade Solid/Assets/_Scripts/BossHP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHP : MonoBehaviour {
+
+    [SerializeField]
+    private int iHealth = 0;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        iHealth = this.transform.childCount;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Transform t = this.transform.GetChild(1);
+            t.gameObject.SetActive(false);
+            iHealth--;
+        }
+	}
+
+}
