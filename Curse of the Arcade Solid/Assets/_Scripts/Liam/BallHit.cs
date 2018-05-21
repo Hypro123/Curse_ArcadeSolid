f68276b30fc426ef90432969fc781f272b03a49e
AIE\s162871 <liamcantfly@gmail.com>
2018-05-21 12:20:12 +1000

    the Ball Hit works

---
+++ Curse of the Arcade Solid/Assets/_Scripts/Liam/BallHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHit : MonoBehaviour
+{
+    private new GameObject gameObject;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "DesstroyAble")
+        {
+            //Debug.Log("C# is fuck");
+            collision.gameObject.GetComponent<ChildBlock>().setThisInactive();
+
+        }
+    }
+}
