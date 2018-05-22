3b76afbf3d04c2efbe5d24d0c3c9676d36997f75
AIE\s171649 <eddie.ultra@gmail.com>
2018-05-22 14:57:04 +1000

    added main menu music script

---
+++ Curse of the Arcade Solid/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip clipIntro;
+    [SerializeField]
+    [Range(0, 1.0f)]
+    private float vol = 0.5f;
+
+    private AudioSource a;
+
+    void Awake ()
+    {
+        a = this.GetComponent<AudioSource>();
+        a.clip = clipIntro;
+        a.loop = true;
+        a.Play();
+    }
+}
