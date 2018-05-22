8c21ca66a2f3b0b2fc1f894ffb18d0a20bcc3843
AIE\s162871 <liamcantfly@gmail.com>
2018-05-22 15:09:36 +1000

    no message

--- Curse of the Arcade Solid/Assets/_Scripts/Liam/CreateBall.cs
+++ Curse of the Arcade Solid/Assets/_Scripts/Liam/CreateBall.cs
@@ -7,17 +7,9 @@ public class CreateBall : MonoBehaviour
     public Transform SpawnPosition;
     public GameObject MagicBallPrefab;
     public GameObject MagicBall;
-    public float Timer = 0;
 
-	// Update is called once per frame
-	void Update ()
+    void Awake()
     {
-        Timer++;
-	    if(Timer == 100)
-        {
-            MagicBall = Instantiate(MagicBallPrefab) as GameObject;
-            //MagicBall.transform.position = GameObject.FindGameObjectWithTag("Ball Spawner");
-            Timer = 0;
-        }
-	}
+        MagicBall = Instantiate(MagicBallPrefab, SpawnPosition) as GameObject;
+    }
 }
